737afd51d4e1b6aec1444fb76e9b4689d1b58766
zchakl <zacharyhakl@gmail.com>
2022-09-17 22:52:23 -0500

    small changes now on to formatter

--- lua/zchakl/colorscheme.lua
+++ lua/zchakl/colorscheme.lua
@@ -1,12 +1,3 @@
--- Example config in Lua
---vim.g.tokyonight_style = "storm"
---vim.g.tokyonight_italic_functions = true
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
--- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
-
-
 vim.cmd [[
 try
   colorscheme tokyonight  
@@ -17,11 +8,3 @@ endtry
 ]]
 
 vim.g.tokyonight_style = 'night'
-
-
-
---require("transparent").setup({
---  enable = false
---})
-
-
