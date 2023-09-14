4c5e4422a655dcc80b795c9059d890ec0f0332c5
zchakl <zacharyhakl@gmail.com>
2023-09-13 19:49:34 -0500

    indent

--- lua/zchakl/options.lua
+++ lua/zchakl/options.lua
@@ -22,20 +22,20 @@ local options = {
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
   autoindent = true,                       -- auto indenting
   copyindent = true,
   preserveindent = true,
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                  -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
+  cursorline = true,         -- highlight the current line
+  number = true,             -- set numbered lines
+  relativenumber = true,     -- set relative numbered lines
+  numberwidth = 4,           -- set number column width to 2 {default 4}
+  signcolumn = "yes",        -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,              -- display lines as one long line
+  scrolloff = 8,             -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  guifont = "monospace:h17", -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append "c"
